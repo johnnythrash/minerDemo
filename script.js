39531ca0201cb8083906cc9ad84cd442496bf944
johnnythrash <chris.garofolo@gmail.com>
2019-03-22 06:35:03 -0400

    add random coin generation

--- script.js
+++ script.js
@@ -16,14 +16,17 @@ const config = {
     }
   };
   
-  let player, topLayer, tiles, groundObj, topObj;
+  let player, topLayer, tiles, groundObj, topObj, coinObj, coins, score;
   
   const game = new Phaser.Game(config);
   const gameWidth = game.config.width, gameHeight = game.config.height;
-  
+  let generateRandom = (min,max) =>{
+    return Math.floor(Math.random() * (max + min) + min);
+  };
   
   function preload(){
     this.load.image('gradient', 'assets/retrogradient.png');
+    this.load.image('coin','assets/sprites/star-coin.png');
     this.load.spritesheet('man', 'assets/sprites/adventurer-Sheet.png', {frameWidth: 50, frameHeight: 37 });
     this.load.spritesheet('tiles', 'assets/sprites/tiles.png',{frameWidth: 70, frameHeight: 70 });
   }
@@ -34,23 +37,36 @@ const config = {
     // add background image
     this.add.image(224,300,'gradient');
    
-    let createSpriteGroup = (x,y,sprite,frame,objectType,objectGroup)=>{
+    let createSpriteGroup = (x,y,sprite,frame,objectType,objectGroup, height=false)=>{
       objectGroup = objectType.create(x,y,sprite,frame);
       objectGroup.width = 35; 
       objectGroup.height = 35;
+      if (height === true){
+        objectGroup.displayHeight = 32;
+        objectGroup.displayWidth = 32;
+      }
       return objectGroup;
-    }
+    };
    
    
     topLayerGroup = this.physics.add.staticGroup();
     dirtLayerGroup = this.physics.add.staticGroup();
-  
+    coinGroup = this.physics.add.staticGroup();
+     
+    // generate coins 
+    for (let i = 0; i < 10; i++)
+    {
+      let x = generateRandom(0,490);
+      let y = generateRandom(210,630);
+      createSpriteGroup(x,y,'coin',[1],coinGroup, 'coinObj', true);
+    }
+
     // draw top layer
     for (let x = 35; x < 490; x+=35)
     {
       createSpriteGroup(x, 210, 'tiles', [2], topLayerGroup, 'topObj');
     }
-    
+
     // generate ground layers  
     for (let i = 0, y = 245; i < 11; i++)
     {
@@ -96,22 +112,24 @@ const config = {
       repeat: -1
     })
   
-      // set up cursor
-      cursors = this.input.keyboard.createCursorKeys();
-  
-    // collider between player and ground
+    // set up cursor
+    cursors = this.input.keyboard.createCursorKeys();
+
+     
+    // set up cursor
+    cursors = this.input.keyboard.createCursorKeys();
+   
+    // collider function between player and ground
     function digDirt(player, group) {
       if (cursors.shift.isDown){
       group.disableBody(this, this);
       }
     }
+    
+    // add colliders
     this.physics.add.collider(player, topLayerGroup, digDirt);
     this.physics.add.collider(player, dirtLayerGroup, digDirt);
-  
-   
-  
-    // set up cursor
-    cursors = this.input.keyboard.createCursorKeys();
+
   
   }
   
